2d96a5e1e98dd5840ec6b5cc1f759b1fd1802648
Togootsch Sara <Togootsch@mosallem.me>
2019-04-08 23:52:21 +0200

    Togootsch: Customer Class init

---
+++ src/Customer.java
@@ -0,0 +1,26 @@
+
+public class Customer {
+int customerID;
+int customerIa;
+String Firstname;
+
+public Customer() {
+	
+}
+
+public int getCustomerID() {
+	return customerID;
+}
+
+public void setCustomerID(int customerID) {
+	this.customerID = customerID;
+}
+
+public String getFirstname() {
+	return Firstname;
+}
+
+public void setFirstname(String firstname) {
+	Firstname = firstname;
+}
+}
